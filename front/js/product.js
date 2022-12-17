96eb62c09276bc0f298971b8b83f3914d570ead7
kgp82 <grandpierre.kevin@gmail.com>
2022-12-17 23:22:17 +0100

    Redirection vers la page du produit sélectionné création du fichier products.js

---
+++ front/js/product.js
@@ -0,0 +1,77 @@
+
+let params = new URLSearchParams(document.location.search);
+let id = params.get("id");
+console.log(id)
+
+
+// fonction fetch pour obtenir les données de l'API
+function getProducts()
+{
+    fetch("http://localhost:3000/api/products/" + id)
+        .then(function(res) {
+            if (res.ok) {
+                return res.json()
+            }
+        })
+        .then((res) => {
+            return pageProduct(res);
+        })
+        .catch((error) => console.log(error))
+}
+
+function pageProduct(product)
+{
+    const itemImg = document.querySelector(".item__img");
+
+    // créer une image
+    const img = document.createElement("img");
+    itemImg.appendChild(img);
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+
+    // afficher le titre
+    const title = document.querySelector("#title");
+    title.textContent = product.name;
+
+    // afficher le prix
+    const prix = product.price;
+    const priceProduct = document.querySelector("#price");
+    priceProduct.textContent = prix;
+
+    // afficher la description
+    const description = product.description;
+    const p = document.querySelector("#description");
+    p.textContent = description;
+
+    // afficher les couleurs dans la liste déroulante
+    const color = product.colors;
+    const colorSelect = document.querySelector("#colors");
+    color.forEach((color) => {
+        const option = document.createElement("option");
+        option.value = color;
+        option.textContent = color;
+        colorSelect.appendChild(option);
+    });
+}
+
+function addToCart(product)
+{
+    const button = document.querySelector("#addToCart")
+    button.addEventListener("click", (event) => {
+        const color = document.querySelector("#colors").value
+        const quantity = document.querySelector("#quantity").value
+
+        if (color == '') {
+            alert("Veuillez sélectionner une couleur")
+            return
+        } else if (quantity == 0) {
+            alert("Veuillez sélectionner une quantité")
+            return
+        } else if (color == '' || quantity == 0) {
+            alert("Veuillez sélectionner une couleur et un quantité")
+            return
+        }
+    })
+}
+
+getProducts();
