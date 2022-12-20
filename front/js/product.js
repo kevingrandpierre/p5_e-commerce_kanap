bc4b58e7d58a249d90f4fb99cd7c89375ef36422
kgp82 <grandpierre.kevin@gmail.com>
2022-12-20 16:51:44 +0100

    Gestion du localStorage : si le produit n'existe pas on l'ajoute, si le produit existe on modifie la quantité.

--- front/js/product.js
+++ front/js/product.js
@@ -1,90 +1,107 @@
-
-let params = new URLSearchParams(document.location.search);
-let id = params.get("id");
-console.log(id)
+let params = new URLSearchParams( document.location.search );
+let id = params.get( "id" );
+console.log( id )
 
 
 // fonction fetch pour obtenir les données de l'API
-function getProducts()
-{
-    fetch("http://localhost:3000/api/products/" + id)
-        .then(function(res) {
-            if (res.ok) {
+function getProducts () {
+    fetch( "http://localhost:3000/api/products/" + id )
+        .then( function ( res ) {
+            if ( res.ok ) {
                 return res.json()
             }
-        })
-        .then((res) => {
-            return pageProduct(res);
-        })
-        .catch((error) => console.log(error))
+        } )
+        .then( ( res ) => {
+            return pageProduct( res );
+        } )
+        .catch( ( error ) => console.log( error ) )
 }
 
-function pageProduct(product)
-{
-    const itemImg = document.querySelector(".item__img");
+// fonction pour créer les éléments du DOM
+function pageProduct ( product ) {
+    const itemImg = document.querySelector( ".item__img" );
 
     // créer une image
-    const img = document.createElement("img");
-    itemImg.appendChild(img);
+    const img = document.createElement( "img" );
+    itemImg.appendChild( img );
     img.src = product.imageUrl;
     img.alt = product.altTxt;
 
     // afficher le titre
-    const title = document.querySelector("#title");
+    const title = document.querySelector( "#title" );
     title.textContent = product.name;
 
     // afficher le prix
     const prix = product.price;
-    const priceProduct = document.querySelector("#price");
+    const priceProduct = document.querySelector( "#price" );
     priceProduct.textContent = prix;
 
     // afficher la description
     const description = product.description;
-    const p = document.querySelector("#description");
+    const p = document.querySelector( "#description" );
     p.textContent = description;
 
     // afficher les couleurs dans la liste déroulante
     const color = product.colors;
-    const colorSelect = document.querySelector("#colors");
-    color.forEach((color) => {
-        const option = document.createElement("option");
+    const colorSelect = document.querySelector( "#colors" );
+    color.forEach( ( color ) => {
+        const option = document.createElement( "option" );
         option.value = color;
         option.textContent = color;
-        colorSelect.appendChild(option);
-    });
+        colorSelect.appendChild( option );
+    } );
 }
 
 // fonction d'écoute au click pour ajouter le produit au panier
-function addToCart(product)
-{
-  const button = document.querySelector("#addToCart")
-    button.addEventListener("click", (event) => {
-        const color = document.querySelector("#colors").value
-        const quantity = document.querySelector("#quantity").value
-        if (color == '') {
-            alert("Veuillez sélectionner une couleur")
+function addToCart () {
+    const button = document.querySelector( "#addToCart" )
+    button.addEventListener( "click" , ( event ) => {
+        const color = document.querySelector( "#colors" ).value
+        const quantity = document.querySelector( "#quantity" ).value
+        // boucle pour la sélection d'une couleur et d'une quantité reste sur la page product si l'une des conditions n'est pas remplie
+        if ( color == '' ) {
+            alert( "Veuillez sélectionner une couleur" )
             return
-        } else if (quantity == 0) {
-            alert("Veuillez sélectionner une quantité")
+        }
+        else if ( quantity == 0 ) {
+            alert( "Veuillez sélectionner une quantité" )
             return
-        } else {
-            //redirection vers le pannier
-            if(confirm("Voulez-vous consulter votre panier ?")) {
-                window.location.href = "cart.html";
-            } else {
-                // si cancel, on reste sur la page
-                return
+        }
+        const currentCart = JSON.parse( localStorage.getItem( "cart" ) )
+
+        const cart = {
+            id: id ,
+            color: color ,
+            quantity: Number( quantity )
+        }
+
+        if ( currentCart == null ) {
+            const currentCart = []
+            currentCart.push( cart )
+            localStorage.setItem( "cart" , JSON.stringify( currentCart ) )
+        }
+
+        if ( currentCart != null ) {
+            const findProduct = currentCart.find( ( product ) => product.id == id && product.color == color )
+            if ( findProduct ) {
+                findProduct.quantity += Number( quantity )
+                localStorage.setItem( "cart" , JSON.stringify( currentCart ) )
+            }
+            else if ( !findProduct ) {
+                {
+                    currentCart.push( cart )
+                    localStorage.setItem( "cart" , JSON.stringify( currentCart ) )
+
+                }
+            }
+            else {
+                currentCart.push( cart )
+                localStorage.setItem( "cart" , JSON.stringify( currentCart ) )
             }
         }
-        const product = {
-            id,
-            color,
-            quantity
-        }
-        localStorage.setItem(id, JSON.stringify(product))
-    })
+        window.location.href = "cart.html"
+    } )
 }
-
 
 
 getProducts();
